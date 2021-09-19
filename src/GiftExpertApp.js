980a372ee3d0d56bf7a6bb48d6027541de814ca9
jantonio2 <jesusrochavedia@gmail.com>
2021-09-19 13:24:35 -0400

    Se agreagó las categorías

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GiftExpertApp = () => {
+
+  // const categories = ['One Punch', 'Smuria X', 'Dragon Ball']
+  const [categories, setCategories] = useState(['One Punch', 'Smuria X', 'Dragon Ball'])
+  
+  const handleAdd = () => {
+    // setCategories([...categories, 'Game of Thrones']);
+    setCategories(categs => [...categs, 'Game of Thrones']);
+  };
+  
   return (
     <>
       <h2>GiftExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {
+          categories.map(category => {
+            return <li key = {category}> {category} </li>
+          })
+        }
+      </ol>
     </>
   )
 }
