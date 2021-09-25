500781adedc687cd092ebb26b926fb719112eb8e
jantonio2 <jesusrochavedia@gmail.com>
2021-09-25 18:46:11 -0400

    Testeando el componente AddCategory, snapshot creado

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import { AddCategory } from '../../components/AddCategory';
+
+describe('Pruebas en <AddCategory />', () => {
+
+  const setCategories = () => {};
+
+  test('debe de mostrarse correctamente', () => {
+    const wrapper = shallow(<AddCategory setCategories = {setCategories}/>);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+});
