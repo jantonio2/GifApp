d8ca32ac0910b72416b8c41bc7f82a68bf354c6c
jantonio2 <jesusrochavedia@gmail.com>
2021-09-25 13:51:06 -0400

    Creando propTypes en GifGridItem y actualizando el snapshot del test

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -2,8 +2,16 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from './../../components/GifGridItem';
 
 describe('Prueba en el componente GifGridItem', () => {
+
+  const title = "Game of Thrones";
+  const url = "https://localhost";
+
   test('debe de mostrar el componente correctamente', () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(
+      <GifGridItem 
+        title = {title}
+        url = {url}
+      />);
 
     expect(wrapper).toMatchSnapshot();
   });
