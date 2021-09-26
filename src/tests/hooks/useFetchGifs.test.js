7dce1a51cb50f1a571886a0e9035df8ac82c348b
jantonio2 <jesusrochavedia@gmail.com>
2021-09-25 21:50:39 -0400

    Utilizando waitForNextUpdate para probar el custom hook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,11 +3,24 @@ import {renderHook} from '@testing-library/react-hooks';
 import { useFetchGifs } from '../../hooks/useFetchGifs';
 
 describe('Pruebas en el hook useFetchGifs', () => {
-  test('debe de retornar el estado inicial', () => {
-    const {result} = renderHook(() => useFetchGifs('Game of Thrones'));
+  test('debe de retornar el estado inicial', async() => {
+    const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Game of Thrones'));
     const {data, loading} = result.current;
     
+    await waitForNextUpdate();
+
     expect(data).toEqual([]);
     expect(loading).toBe(true);
   });
+
+  test('debe de retornar un arreglo de imagenes y el loading en false', async() => {
+    const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Game of Thrones'));
+    await waitForNextUpdate();
+
+    const {data, loading} = result.current;
+    
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
+  });
+  
 });
